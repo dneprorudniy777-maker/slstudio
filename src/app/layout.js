ff7fc18dc2b1a1749a53db86342bf47d992e452b
Serhii_Lazariev <dneprorudniy777@gmail.com>
2026-05-29 17:47:55 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import "./globals.css";
 
-const inter = Inter({
-  subsets: ['latin']
+const outfit = Outfit({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
 })
 
 export const metadata = {
@@ -16,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${inter.className} min-h-screen flex flex-col`}>
+            <body className={`${outfit.className} min-h-screen flex flex-col`}>
                 <Header />
                 <main className="container flex-1">
                     {children}
