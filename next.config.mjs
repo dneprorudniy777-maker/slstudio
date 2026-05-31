72eee84f7b07ce9bc0972f85e095b579b0f5861d
Serhii_Lazariev <dneprorudniy777@gmail.com>
2026-05-31 21:40:19 +0200

    23

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'slstudio.pro',
+          },
+        ],
+        destination: 'https://www.slstudio.pro/$1',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
